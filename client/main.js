86e8fe02cf8017b76dcc42c61872ae3233cc04b3
Christopher Paretti <cparetti@samsungaccelerator.com>
2017-04-19 18:43:00 -0400

    Getting into backend

--- client/main.js
+++ client/main.js
@@ -1,4 +1,39 @@
-import someDefault from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('Log from /client/main.js');
-console.log(someDefault);
+
+const players = [{
+  _id: '1',
+  name: 'Chris',
+  score: 19
+},{
+  _id: '2',
+  name: 'Johnson',
+  score: 4
+},{
+  _id: '3',
+  name: 'Farty',
+  score: -2
+}];
+
+const renderPlayers = function (playersList) {
+
+  return playersList.map(function(player) {
+    return <p key={player._id}>{player.name} has {player.score} points</p>
+  });
+};
+
+Meteor.startup(function () {
+  let title = 'Score Keep';
+  let name = 'chrisp';
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      <p >This is my second p.</p>
+      {renderPlayers(players)}
+    </div>
+  );
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
